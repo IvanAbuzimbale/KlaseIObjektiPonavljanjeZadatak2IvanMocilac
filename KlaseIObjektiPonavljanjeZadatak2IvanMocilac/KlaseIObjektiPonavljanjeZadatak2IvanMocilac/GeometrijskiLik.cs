eb0ffb9568b9a20af03e53dd5f7bc931ae06dc91
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-10-17 11:59:05 +0200

    Add files via upload

---
+++ KlaseIObjektiPonavljanjeZadatak2IvanMocilac/KlaseIObjektiPonavljanjeZadatak2IvanMocilac/GeometrijskiLik.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlaseIObjektiPonavljanjeZadatak2IvanMocilac
+{
+    class GeometrijskiLik
+    {
+        public int BrojStranica
+        {
+            get;
+            set;
+        }
+        
+        public double[] Stranice
+        {
+            get;
+            set;
+        }
+
+        public GeometrijskiLik(int brojStranica)
+        {
+            BrojStranica = brojStranica;
+        }
+
+        public void UcitajStranice()
+        {
+            Stranice = new double[BrojStranica];
+            for (int i = 0; i < BrojStranica; i++)
+            {
+                Console.Write($"Unesite {i + 1}. stranicu: ");
+                Stranice[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public double Opseg()
+        {
+            double vrati = 0;
+            foreach(var broj in Stranice)
+            {
+                vrati += broj;
+            }
+            return vrati;
+        }
+    }
+}
---
+++ KlaseIObjektiPonavljanjeZadatak2IvanMocilac/KlaseIObjektiPonavljanjeZadatak2IvanMocilac/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlaseIObjektiPonavljanjeZadatak2IvanMocilac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trokut trokut = new Trokut();
+            Cetverokut cetverokut = new Cetverokut();
+
+            Console.WriteLine("Trokut");
+            trokut.UcitajStranice();
+            double opsegTrokuta = trokut.Opseg();
+            Console.WriteLine();
+
+            Console.WriteLine("Četverokut");
+            cetverokut.UcitajStranice();
+            double opsegCetverkuta = cetverokut.Opseg();
+            Console.WriteLine();
+
+            Console.WriteLine($"Opseg trokuta je: {opsegTrokuta}");
+            Console.WriteLine($"Opseg kvadrata je: {opsegCetverkuta}");
+            Console.ReadKey();
+        }
+    }
+}
